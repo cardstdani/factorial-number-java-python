538f3ce0addc26928bd9ccd00f524b9be9293cdf
cardstdani <72665778+cardstdani@users.noreply.github.com>
2020-10-11 11:16:58 +0200

    Update MainClass.py

--- MainClass.py
+++ MainClass.py
@@ -1,1 +1,18 @@
+#Recursion method
+def factorialRecursionMethod(n):
+  if n == 0:
+    return 1
+  else:
+    return n*factorialRecursionMethod(n-1)
 
+#For loop method
+def factorialForLoopMethod(n):
+  if n == 0:
+    return 1
+  else:
+    result = 1
+    for i in range(1, n+1):
+      result *= i
+    return result
+
+print(factorialForLoopMethod(5))
